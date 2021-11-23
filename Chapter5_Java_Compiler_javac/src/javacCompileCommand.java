b5c56b6fac438f5858f52275c45e9a74077c3499
Pranay Singh <pranay.singh.1393@gmail.com>
2021-11-23 20:30:25 +0530

    javac Compile Command
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Chapter5_Java_Compiler_javac/src/javacCompileCommand.java
@@ -0,0 +1,15 @@
+
+public class javacCompileCommand {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi! Good Morning.");
+
+	}
+
+}
+/*
+ * We can compile the above file using this command: $ javac HelloWorld.java
+ * This produces a file called "HelloWorld.class", which we can then run as
+ * follows: $ java HelloWorld Hello world!
+ */
