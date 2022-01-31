baf2760143633a91a18bf4c87cded46de6eddd06
Pranay Singh <pranay.singh.1393@gmail.com>
2022-01-31 23:55:00 +0530

    Getting Length of string via length()
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Chapter9_Strings/src/Getting_length_of_String.java
@@ -0,0 +1,21 @@
+
+public class Getting_length_of_String {
+
+	public static void main(String[] args) {
+		/*
+		 * In order to get the length of a String object, call the length() method on
+		 * it. The length is equal to the number of UTF-16 code units (chars) in the
+		 * string.
+		 */
+		String str = "Hello, World!";
+		System.out.println(str.length()); // 13
+
+		// We can also use a Stream of codepoints, as of Java 8:
+		System.out.println(str.codePoints().count());// 13
+		System.out.println(str.codePointAt(0));// Unicode of char at 0 index
+		System.out.println(str.codePointBefore(1));// Unicode of char before 1 index
+		System.out.println(str.codePointCount(0, 2));// length between the given index range
+
+	}
+
+}
